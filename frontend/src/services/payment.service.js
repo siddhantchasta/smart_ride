53c59a8acbac87c1cf5ca00d8e9911d82906ddb9
Siddhant Chasta <siddhantchasta4199@gmail.com>
2026-03-23 03:14:54 +0530

    Remove frontend folder

--- frontend/src/services/payment.service.js
+++
@@ -1,7 +0,0 @@
-import API from "./api";
-
-export const createOrder = (data) =>
-  API.post("/payments/create-order", data);
-
-export const verifyPayment = (data) =>
-  API.post("/payments/verify", data);
--- frontend/src/services/user.service.js
+++
@@ -1,7 +0,0 @@
-import API from "./api";
-
-export const saveLocation = (data) =>
-  API.post("/user/location", data);
-
-export const getLocation = () =>
-  API.get("/user/location");
