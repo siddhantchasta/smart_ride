452baf0a7302858991376fe4a6b7455903d2d3d2
Siddhant Chasta <siddhantchasta4199@gmail.com>
2026-03-27 00:25:33 +0530

    added missing distance b/w routes logic

--- backend/src/services/user.service.js
+++ backend/src/services/user.service.js
@@ -42,7 +42,7 @@ const { getCoordinates, calculateDistance } = require('./maps.service');
 const addUserLocation = async (data) => {
   const { user_id, route_id } = data;
 
-  // 🔹 1. Get route
+  // Get route
   const routeRes = await pool.query(
     `SELECT * FROM routes WHERE id = $1 AND is_active = true`,
     [route_id]
@@ -53,16 +53,21 @@ const addUserLocation = async (data) => {
   }
 
   const route = routeRes.rows[0];
+  const distance = await calculateDistance(
+    route.start_location,
+    route.end_location,
+  );
 
-  // 🔹 2. Save into user_locations
+  // Save into user_locations
+
   const result = await pool.query(
     `INSERT INTO user_locations 
     (user_id,
      pickup_address, pickup_lat, pickup_lng,
      drop_address, drop_lat, drop_lng,
      distance_km,
-     route_id)
-     VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9)
+     route_id, duration_text)
+     VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10)
      RETURNING *`,
     [
       user_id,
@@ -72,8 +77,9 @@ const addUserLocation = async (data) => {
       route.end_location,
       route.end_lat,
       route.end_lng,
-      null, // optional for now
-      route.id
+      distance.distance_value / 1000,
+      route.id,
+      distance.duration_text,
     ]
   );
 
