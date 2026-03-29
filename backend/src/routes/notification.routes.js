db1235495d277c229b6d5920713a7801353d980a
Siddhant Chasta <siddhantchasta4199@gmail.com>
2026-03-29 13:49:05 +0530

    removed the unused createNotification function

--- backend/src/routes/notification.routes.js
+++ backend/src/routes/notification.routes.js
@@ -2,11 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const {
-  createNotification,
   fetchNotifications,
 } = require('../controllers/notification.controller');
 
-router.post('/', createNotification);
 router.get('/:user_id', fetchNotifications);
 
 module.exports = router;
