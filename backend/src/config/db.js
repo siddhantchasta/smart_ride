aa8ac0da72dfb84ccd74dce177e5299f8bb6b37b
Siddhant Chasta <siddhantchasta4199@gmail.com>
2026-04-04 02:05:14 +0530

    fix: database connection logic from supabase

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,10 +1,24 @@
-const { Pool } = require("pg");
+// for local postgres database use this : 
+const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
 });
 
 module.exports = pool;
+
+// otherwise if your database is on supabase then use the below code and comment the above one 
+// const { Pool } = require("pg");
+
+// const pool = new Pool({
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: {
+//     rejectUnauthorized: false,
+//   },
+// });
+
+// module.exports = pool;
